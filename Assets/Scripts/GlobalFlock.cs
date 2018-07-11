57b90cdccad54edf7d1db6a20d0ae81475d323af
sangyeonK <sovietist@hotmail.com>
2018-07-12 03:04:29 +0900

    [feature][integration] 물고기 떼 구현
    * https://github.com/Nexters/DeepDeepSea/commit/a792b86f5a0b36c1bc63af6d79806ad2ce43d04a

---
+++ Assets/Scripts/GlobalFlock.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//
+//https://www.youtube.com/watch?v=eMpI1eCsIyM
+
+public class GlobalFlock: MonoBehaviour {
+
+    public GameObject fishPrefab;
+    public GameObject goalPrefab;
+
+    public static int tankSize = 5;
+    static int numFish = 10;
+
+    public static GameObject[] allFish = new GameObject[numFish];
+
+    public static Vector3 goalPos = Vector3.zero;
+
+
+	// Use this for initialization
+	void Start () {
+		for (int i=0; i< numFish; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-tankSize, tankSize),
+                Random.Range(-tankSize, tankSize),
+                Random.Range(-tankSize, tankSize));
+            Quaternion quaternion = Quaternion.identity;
+            quaternion.eulerAngles = new Vector3(0, 90, 0);
+
+
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, quaternion);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Random.Range(0, 10000) < 50)
+        {
+            goalPos = new Vector3(Random.Range(-tankSize, tankSize),
+                Random.Range(-tankSize, tankSize),
+                Random.Range(-tankSize, tankSize));
+
+            goalPrefab.transform.position = goalPos;
+        }
+	}
+}
