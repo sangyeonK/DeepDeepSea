d46826626e4a4c49644fcd1c90c283801b2bd0ef
sangyeonK <sovietist@hotmail.com>
2018-08-18 01:38:34 +0900

    [BUILD] buildscript 추가
     - batchmode 로 빌드하기 : https://github.com/Nexters/DeepDeepSea/wiki

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+
+public class BuildScript{
+
+    public static void BuildAndroid()
+    {
+        Build(BuildTarget.Android);
+    }
+
+    public static void BuildIOS()
+    {
+        Build(BuildTarget.iOS);
+    }
+
+    public static void BuildWindows()
+    {
+        Build(BuildTarget.StandaloneWindows);
+    }
+
+    static void Build(BuildTarget target)
+    {
+        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, Environment.GetCommandLineArgs().Last(), target, BuildOptions.None);
+    }
+
+}
