617113ede639aa6fa1b2bfa1c1bc5b21e304ebdd
seohakyong <smartdragon417@gmail.com>
2018-08-18 19:21:03 +0900

    맵타입랜덤/테스트

---
+++ Assets/Scripts/map/rotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+    public GameObject go;
+    float angle = 360.0f; // Degree per time unit
+    float time = 1.0f; // Time unit in sec
+  //  Vector3 axis = Vector3.up; // Rotation axis, here it the yaw axis
+    Vector3 axis2 = Vector3.forward;
+    private void Update()
+    {
+        go.GetComponent<Transform>().RotateAround(Vector3.zero, axis2, angle * Time.deltaTime / time);
+    }
+
+}
