c5234979ce2c49fbdff314ba66bd1f015da01a3a
sangyeonK <sovietist@hotmail.com>
2018-08-19 17:14:52 +0900

    [FIX] gameoverPanel 에서 text component 연결방법 변경

--- Assets/Scripts/UI/UIGameOverPanel.cs
+++ Assets/Scripts/UI/UIGameOverPanel.cs
@@ -7,33 +7,17 @@ using Assets.Scripts.Util;
 
 public class UIGameOverPanel : MonoBehaviour {
 
-    Text playTime;
-    Text stage;
-
-    private void Awake()
-    {
-        foreach(var textComponent in GetComponentsInChildren<Text>())
-        {
-            if(textComponent.name == "PlayTime")
-            {
-                playTime = textComponent;
-            }
-            if (textComponent.name == "Stage")
-            {
-                stage = textComponent;
-            }
-
-        }
-    }
+    public Text playTime;
+    public Text playDepth;
 
     public void SetData(int playTime, int playDepth)
     {
         TimeSpan ts = new TimeSpan(0, 0, playTime);
         
         this.playTime.text = ts.ToString();
-		this.stage.text = Define.DepthToName(playDepth);
+        this.playDepth.text = playDepth.ToString();
 
-		FileManager.Instance.Save(playTime, playDepth);
+        FileManager.Instance.Save(playTime, playDepth);
     }
 
     
