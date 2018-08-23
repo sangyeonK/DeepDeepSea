f8fcae0dd1039036a15239f23d4989bff0f323ff
seohakyong <smartdragon417@gmail.com>
2018-08-24 04:13:41 +0900

    레퍼런스정리/뉴맵3/맵매니저수정

--- Assets/Scripts/map/rotate.cs
+++ Assets/Scripts/map/rotate.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class rotate : MonoBehaviour {
 
-    public GameObject go;
-    float angle = 360.0f; // Degree per time unit
-    float time = 1.0f; // Time unit in sec
-  //  Vector3 axis = Vector3.up; // Rotation axis, here it the yaw axis
-    Vector3 axis2 = Vector3.forward;
-    private void Update()
+    public float rotFl;
+    // Use this for initialization
+    void Start()
     {
-        go.GetComponent<Transform>().RotateAround(Vector3.zero, axis2, angle * Time.deltaTime / time);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, 0, Mathf.PingPong(Time.time * 50, rotFl));
+        /*if( check something so that switch occurs){
+        transform.localEulerAngles = new Vector3(0, 0, Mathf.PingPong(Time.time * 50, -rotFl));    
+        }*/
     }
 
 }
