081faaf208208ff577bb5a6f8bb545e4338c3a0f
sangyeonK <sovietist@hotmail.com>
2018-09-02 02:20:13 +0900

    [FIX] title scene 에서 게임종료창 띄우기 수정
     - 다른팝업들이 떠 있을때는 종료창 띄우지 않음

--- Assets/Scripts/TitleSceneManager.cs
+++ Assets/Scripts/TitleSceneManager.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
+using System.Reflection;
+using System.Linq;
 
 public class TitleSceneManager : MonoBehaviour {
 
+    [AttributeUsage(AttributeTargets.Field)]
+    class PopUpAttribute : Attribute { };
+
     [Header("Canvas UI")]
+    [PopUp]
     public RectTransform mTeamView;
+    [PopUp]
     public RectTransform quitPanel;
     public RectTransform startButton;
+    [PopUp]
     public RectTransform playHistoryPanel;
 
+    private List<RectTransform> popups = new List<RectTransform>();
     private void Start()
     {
         StartCoroutine(ActiveStartButton());
+        CollectPopups();
     }
 
     IEnumerator ActiveStartButton()
@@ -43,17 +54,36 @@ public class TitleSceneManager : MonoBehaviour {
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
-            if(quitPanel.gameObject.activeSelf == false)
+            if(popups.All(popup => !popup.gameObject.activeSelf))
             {
                 quitPanel.gameObject.SetActive(true);
             }
-
         }
     }
 
     public void OnQuitButton()
     {
         Application.Quit();
+    }
+
+    private void CollectPopups()
+    {
+        popups.Clear();
+        FieldInfo[] fields = GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        foreach (FieldInfo field in fields)
+        {
+            foreach (Attribute attr in field.GetCustomAttributes(true))
+            {
+                PopUpAttribute popup = attr as PopUpAttribute;
+                if (popup != null)
+                {
+                    RectTransform obj = field.GetValue(this) as RectTransform;
+                    if (obj != null)
+                        popups.Add(obj);
+                    
+                }
+            }
+        }
     }
 
 }
