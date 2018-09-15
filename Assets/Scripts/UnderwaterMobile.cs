c173445cfc07308f57b77fd5eaa53c12bbc3ca96
sangyeonK <sovietist@hotmail.com>
2018-09-16 00:37:26 +0900

    [FIX] 물속 휴대폰 위치 조정
    기본상태는 화면 밑에 잠겨서 안보이게 됨

--- Assets/Scripts/UnderwaterMobile.cs
+++ Assets/Scripts/UnderwaterMobile.cs
@@ -2,25 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnderwaterMobile : MonoBehaviour {
-
-    float minY = -10f;
-    float maxY = 0f;
+public class UnderwaterMobile : MonoBehaviour
+{
     public GameObject sprite;
+    public float minY = -12f;
+    public float maxY = 3f;
     public float floatOnFactor = 0.25f;   // 값이 증가할수록 초반에 가파르게 떠오름
     public float floatOffFactor = 3.0f;   // 지정한 값이 클수록 빠르게 아래로 잠수
     private Vector3 rotationAngle;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         rotationAngle = Vector3.forward * Random.Range(60f, 120f);
         transform.localPosition = new Vector2(0.0f, minY);
 
         sprite.GetComponent<SpriteRenderer>().enabled = false;
         GameManager.Instance.AddStartPlayListener(OnStartPlay);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         sprite.transform.Rotate(rotationAngle * Time.deltaTime);
 
         // speed mode 활성화 되면 떠오름..그렇지 않으면 가라앉음
